3f9685ee75f1815e4530471956671db429a42d34
Juraj Noge <juraj.noge@productboard.com>
2024-12-04 10:38:02 +0100

    Day 4

--- src/test/kotlin/AdventOfCodeTasksTest.kt
+++ src/test/kotlin/AdventOfCodeTasksTest.kt
@@ -18,6 +18,11 @@ class AdventOfCodeTasksTest {
         runTaskTest(Instructions(), 161085926, 82045421)
     }
 
+    @Test
+    fun day04() {
+        runTaskTest(Letters(), 2575, 2041)
+    }
+
     private fun runTaskTest(task: AdventOfCodeTask, part1Result: Any, part2Result: Any) {
         assertEquals(part1Result, task.run())
         assertEquals(part2Result, task.run(part2 = true))
